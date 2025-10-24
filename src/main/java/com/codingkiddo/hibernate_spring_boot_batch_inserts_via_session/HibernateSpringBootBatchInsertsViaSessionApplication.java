f6ab212fd71d1d0929d3229f5387ed27686f6a59
Vinod Kumar <vinodkumar@192.168.1.4>
2025-10-24 20:35:11 +0530

    Initial Commit

---
+++ src/main/java/com/codingkiddo/hibernate_spring_boot_batch_inserts_via_session/HibernateSpringBootBatchInsertsViaSessionApplication.java
@@ -0,0 +1,13 @@
+package com.codingkiddo.hibernate_spring_boot_batch_inserts_via_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringBootBatchInsertsViaSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateSpringBootBatchInsertsViaSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingkiddo/hibernate_spring_boot_batch_inserts_via_session/HibernateSpringBootBatchInsertsViaSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingkiddo.hibernate_spring_boot_batch_inserts_via_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSpringBootBatchInsertsViaSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
